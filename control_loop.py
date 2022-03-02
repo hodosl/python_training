75ab9f0e195795a15079bef9fd8ced445b025aab
hodosl <laszlo.hodos@telekom.com>
2022-03-02 16:18:51 +0100

    for

---
+++ control_loop.py
@@ -0,0 +1,13 @@
+print("Begin")
+count = 5
+# for i in range (0,count):
+#     print(i)
+#     print("hello world")
+#     print("hello python")
+# print("EXIT")
+
+for name in ["John", "Jack", "Jane"]:
+    print(name)
+
+for i in [2, 4, 6 ,8]:
+    print(i)
