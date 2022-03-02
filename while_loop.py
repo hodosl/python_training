0b1c4bd16be7fbaa404a0c0f6022d16cb3c83559
hodosl <laszlo.hodos@telekom.com>
2022-03-02 16:46:45 +0100

    while

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+# count = 0
+# while count < 10:
+#     print("hello" + str(count))
+#     count = count + 1
+number = 10
+while number != 0:
+    number = int(input("Adj egy számot: "))
+    print(number*2)
+print("End")
